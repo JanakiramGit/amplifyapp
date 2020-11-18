33e7ebdb5ca44c7d84a5ec48aa1504e416c1f251
Janakiram Sampalli <janakiram.sampalli@gmail.com>
2020-11-18 20:07:42 +1100

    auth added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+
 function App() {
   return (
     <div className="App">
       <div>
-        Hi there agian from version 2!
+        Hi now with Auth!
+
+        <AmplifySignOut/>
       </div>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
